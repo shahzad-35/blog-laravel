cbfe3a393155b1d3544d8d111812421a9bfcd0d9
Shahzad <shahzad.ali@ilsainteractive.com>
2022-03-22 18:11:53 +0500

    Add categories to table on running migration

--- database/migrations/2021_08_23_131840_create_categories_table.php
+++ database/migrations/2021_08_23_131840_create_categories_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Carbon\Carbon;
 
 class CreateCategoriesTable extends Migration
 {
@@ -15,9 +16,10 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('category');    
+            $table->string('category');
             $table->timestamps();
         });
+        $this->saveCategories();
     }
 
     /**
@@ -28,5 +30,17 @@ class CreateCategoriesTable extends Migration
     public function down()
     {
         Schema::dropIfExists('categories');
+    }
+
+    public function saveCategories()
+    {
+        $now =Carbon::now();
+        $categories = json_decode(file_get_contents(base_path('database/migrations/categories.json')), true);
+        foreach (json_decode(file_get_contents(base_path('database/migrations/categories.json')), true) as $key => $category) {
+            $categories[$key]['category'] = $category['category'];
+            $categories[$key]['created_at'] = $categories[$key]['updated_at'] = $now;
+        }
+        \DB::table("categories")->insert(array_values($categories));
+        return true;
     }
 }
